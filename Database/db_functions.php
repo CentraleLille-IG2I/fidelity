aaf194d2196eb2e8a77eaf7225cc64dcefca41aa
Richard-Degenne <richdeg2@gmail.com>
2014-02-23 22:25:03 +0100

    SQL functions
    Added new SQL functions
    - getClientByCard
    - getValidReductions
    - updateClient
    - newReduction
    - updateReduction
    
    Fixed newClient
    
    See in-file documentation for further details.

--- Database/db_functions.php
+++ Database/db_functions.php
@@ -29,6 +29,8 @@
 		global $db;
 		$request = $db->prepare('SELECT * from `Clients`');
 		$request->execute();
+		
+		$toReturn = array();
 		while($data = $request->fetch())
 		{
 			$toReturn[] = array( // Le tableau est ajouté à l'ancien $toReturn
@@ -63,8 +65,49 @@
 	function getClientById($id)
 	{
 		global $db;
-		$request = $db->prepare("SELECT * from `Clients` WHERE `id` = ?");
+		$request = $db->prepare("SELECT * from `Clients` WHERE `id` = ? LIMIT 1");
 		$request->execute(array($id));
+		
+		if($data = $request->fetch())
+		{
+			$request->closeCursor();
+			return array(
+				"id" => $data['id'],
+				"nom" => $data['nom'],
+				"prenom" => $data['prenom'],
+				"numeroCarte" => $data['numeroCarte'],
+				"adresse" => $data['adresse'],
+				"ville" => $data['ville'],
+				"codePostal" => $data['codePostal'],
+				"telephone" => $data['telephone'],
+				"mail" => $data['mail'],
+				"aboMail" => $data['aboMail'],
+				"aboSms" => $data['aboSms'],
+				"cagnotte" => $data['cagnotte'],
+				"dateDeNaissance" => $data['dateDeNaissance'],
+				"interets" => $data['interets']
+				);
+		}
+		else
+		{
+			$request->closeCursor();
+			return E_ERROR;
+		}
+	}
+	
+	/*---------------------------*
+	 * Fonction :	getClientByCard
+	 * Paramètres :	card - Entier
+	 * Retour :		Tableau ou E_ERROR
+	 * Description :	Recherche un client à partir de son numéro de carte (card).
+	 						- Si trouvé, renvoie un tableau associatif contenant ses informations personnelles.
+	 						- Si non trouvé, renvoie E_ERROR.
+	/*---------------------------*/
+	function getClientByCard($card)
+	{
+		global $db;
+		$request = $db->prepare("SELECT * from `Clients` WHERE `numeroCarte` = ? LIMIT 1");
+		$request->execute(array($card));
 		
 		if($data = $request->fetch())
 		{
@@ -135,6 +178,8 @@
 			return E_ERROR;
 		}
 	}
+	
+	
 	// Réductions
 	
 	/*---------------------------*
@@ -160,7 +205,7 @@
 				"fin" => $data['fin']
 				);
 		}
-		$response->closeCursor();
+		$request->closeCursor();
 		return $toReturn;
 	}
 	
@@ -198,6 +243,74 @@
 		}
 	}
 	
+	/*---------------------------*
+	 * Fonction :	getValidReductions
+	 * Paramètres :	id - Entier
+	 * Retour :		Tableau ou E_ERROR
+	 * Description :	Recherche toutes les réductions disponibles pour un client donné
+	/*---------------------------*/
+	function getValidReductions($id)
+	{
+		global $db;
+		$client = getClientById($id);
+		$cagnotte = $client['cagnotte'];
+		echo "SELECT * from `Reductions` WHERE `debut` <= CURDATE() AND `fin` >= CURDATE() AND `cout` <= $cagnotte\n";
+		$request = $db->prepare("SELECT * from `Reductions` WHERE `debut` <= CURDATE() AND `fin` >= CURDATE() AND `cout` <= $cagnotte");
+		$request->execute();
+		
+		$toReturn = array();
+		while($data = $request->fetch())
+		{
+			$toReturn[] = array( // Le tableau est ajouté à l'ancien $toReturn
+				"id" => $data['id'],
+				"description" => $data['description'],
+				"cout" => $data['cout'],
+				"type" => $data['type'],
+				"valeur" => $data['valeur'],
+				"debut" => $data['debut'],
+				"fin" => $data['fin']
+				);
+		}
+		$request->closeCursor();
+		return $toReturn;
+	}
+	
+	/*---------------------------*
+	 * Fonction :	searchReduction
+	 * Paramètres :	pattern - Mixte
+	 				field - Chaîne
+	 * Retour :		Tableau à 2 dimensions
+	 * Description :	Recherche des réduction à partir du champ $field et de la valeur $pattern.
+	 						- Renvoie un tableau contenant toutes les réductions trouvés sous forme de tableaux associatifs.
+	 						- Renvoie E_ERROR si la requête échoue.
+	/*---------------------------*/
+	function searchReduction($pattern,$field)
+	{
+		global $db;
+		if($request = $db->query("SELECT * FROM `Reductions` WHERE ".$field." LIKE ".$db->quote("%".$pattern."%")))
+		{
+			$toReturn = array();
+			while($data = $request->fetch())
+			{
+				$toReturn[] = array(
+				"id" => $data['id'],
+				"description" => $data['description'],
+				"cout" => $data['cout'],
+				"type" => $data['type'],
+				"valeur" => $data['valeur'],
+				"debut" => $data['debut'],
+				"fin" => $data['fin']
+				);
+			}
+			$request->closeCursor();
+			return $toReturn;
+		}
+		else
+		{
+			return E_ERROR;
+		}
+	}
+	
 	
 	/*
 	 * Écriture
@@ -206,37 +319,30 @@
 	// Clients
 	
 	/*---------------------------*
-	 * Fonction :	newClient // NON-FONCTIONNELLE
+	 * Fonction :	newClient
 	 * Paramètres :	toInsert - Tableau
 	 * Retour :		Booléen
-	 * Description :	Insère un nouveau client en Vérifie l'intégrité des champs avant insertion. Renvoie un booléen fonction de la réussite de l'insertion.
+	 * Description :	Insère un nouveau client en vérifiant l'intégrité des champs avant insertion. Renvoie un booléen fonction de la réussite de l'insertion.
 	/*---------------------------*/
-/*	function newClient($toInsert)
+	function newClient($toInsert)
 	{
 		global $db;
-		if(isset($toInsert['numeroDeCarte']) && isset($toInsert['nom']) && isset($toInsert['prenom']) && isset($toInsert['adresse']) && isset($toInsert['ville']) && isset($toInsert['aboMail']) && isset($toInsert['aboSms'])) // Test de la présence des champs obligatoire
+		if(isset($toInsert['numeroCarte']) && isset($toInsert['nom']) && isset($toInsert['prenom']) && isset($toInsert['adresse']) && isset($toInsert['ville']) && isset($toInsert['aboMail']) && isset($toInsert['aboSms'])) // Test de la présence des champs obligatoire
 		{
-			echo "All set.\n";
-			if(!empty($toInsert['numeroDeCarte']) && !empty($toInsert['nom']) && !empty($toInsert['prenom']) && !empty($toInsert['adresse']) && !empty($toInsert['ville']) && ($toInsert['aboMail'] == 0 || $toInsert['aboMail'] == 1) && ($toInsert['aboSms'] == 0 || $toInsert['aboSms'] == 1)) // Si tous les champs obligatoires ne sont pas vides (condition différente pour les booléens : empty renvoie true si $entier = 0)
+			if(!empty($toInsert['numeroCarte']) && !empty($toInsert['nom']) && !empty($toInsert['prenom']) && !empty($toInsert['adresse']) && !empty($toInsert['ville']) && ($toInsert['aboMail'] == 0 || $toInsert['aboMail'] == 1) && ($toInsert['aboSms'] == 0 || $toInsert['aboSms'] == 1)) // Si tous les champs obligatoires ne sont pas vides (condition différente pour les booléens : empty renvoie true si $entier = 0)
 			{
-				echo "All not empty.\n";
 				$fields = "INSERT INTO `Clients`(`cagnotte`"; // On crée le début de la requête
-				$values = "VALUES('0.0'";
+				$values = "VALUES(0.0";
 				
 				foreach($toInsert as $key => $value) // Ensuite, pour chaque champ, on concatène la chaine existante
 				{
 					if(isset($toInsert[$key]) && !empty($toInsert[$key]) || $toInsert[$key] == 0)
 					{
-						echo "Added ".$key." => ".$value."\n";
 						$fields = $fields.",`".$key."`";
 						$values = $values.",:".$key;
 						$valuesArray[$key] = $value;
 					}
 				}
-				echo "\nRequest : ".$fields.") ".$values.")\n";
-				echo "Parameters :<br /><pre>";
-				print_r($valuesArray);
-				echo "</pre>";
 				$request = $db->prepare($fields.") ".$values.")");
 				$request->execute($valuesArray);
 				
@@ -252,15 +358,133 @@
 				}
 			}
 		}
+		return false;
+	}
+	
+	/*---------------------------*
+	 * Fonction :	updateClient
+	 * Paramètres :	toUpdate - Tableau
+	 * Retour :		Booléen
+	 * Description :	Met un jour un client (id) en vérifiant l'intégrité de chaque valeur du tableau toUpdate.
+	/*---------------------------*/
+	function updateClient($id,$toUpdate)
+	{
+		global $db;
+		$toRequest = "UPDATE `Clients` SET id=id"; // Amorce de la requête
+		
+		foreach($toUpdate as $key => $value) // Ensuite, pour chaque champ, on concatène la chaine existante
+		{
+			if(isset($toUpdate[$key]) && !empty($toUpdate[$key]) || $toUpdate[$key] == 0)
+			{
+				$toRequest = $toRequest.", ".$key." = :$key";
+				$valuesArray[$key] = $value;
+			}
+		}
+		$toRequest = $toRequest." WHERE id = :id";
+		$valuesArray["id"] = $id;
+		
+		echo "<pre>\n";
+		print_r($valuesArray);
+		echo $toRequest;
+		echo "</pre>";
+		$request = $db->prepare($toRequest);
+		$request->execute($valuesArray);
+		
+		if($request->rowCount())
+		{
+			$request->closeCursor();
+			return true;
+		}
 		else
 		{
-		
-		}return false; 
-
-		$request = $db->prepare("SELECT * from `Clients` WHERE `id` = ?");
+			$request->closeCursor();
+			return false;
+		}
 	}
-*/	
+	
 	// Réductions
 	
+	/*---------------------------*
+	 * Fonction :	newReduction
+	 * Paramètres :	toInsert - Tableau
+	 * Retour :		Booléen
+	 * Description :	Insère une nouvelle réduction en vérifiant l'intégrité des champs avant insertion. Renvoie un booléen fonction de la réussite de l'insertion.
+	/*---------------------------*/
+	function newReduction($toInsert)
+	{
+		global $db;
+		if(isset($toInsert['description']) && isset($toInsert['cout']) && isset($toInsert['type']) && isset($toInsert['valeur']) && isset($toInsert['debut']) && isset($toInsert['fin'])) // Test de la présence des champs obligatoire
+		{
+			if(!empty($toInsert['description']) && !empty($toInsert['cout']) && !empty($toInsert['type']) && !empty($toInsert['valeur']) && !empty($toInsert['debut']) && !empty($toInsert['fin'])) // Si tous les champs obligatoires ne sont pas vides
+			{
+				$fields = "INSERT INTO `Reductions`(`id`"; // On crée le début de la requête
+				$values = "VALUES(''";
+				
+				foreach($toInsert as $key => $value) // Ensuite, pour chaque champ, on concatène la chaine existante
+				{
+					if(isset($toInsert[$key]) && !empty($toInsert[$key]))
+					{
+						$fields = $fields.",`".$key."`";
+						$values = $values.",:".$key;
+						$valuesArray[$key] = $value;
+					}
+				}
+				$request = $db->prepare($fields.") ".$values.")");
+				$request->execute($valuesArray);
+				
+				if($request->rowCount())
+				{
+					$request->closeCursor();
+					return true;
+				}
+				else
+				{
+					$request->closeCursor();
+					return false;
+				}
+			}
+		}
+		return false;
+	}
 	
+	/*---------------------------*
+	 * Fonction :	updateReduction
+	 * Paramètres :	toUpdate - Tableau
+	 * Retour :		Booléen
+	 * Description :	Met un jour une réduction (id) en vérifiant l'intégrité de chaque valeur du tableau toUpdate.
+	/*---------------------------*/
+	function updateReduction($id,$toUpdate)
+	{
+		global $db;
+		$toRequest = "UPDATE `Reductions` SET id=id"; // Amorce de la requête
+		
+		foreach($toUpdate as $key => $value) // Ensuite, pour chaque champ, on concatène la chaine existante
+		{
+			if(isset($toUpdate[$key]) && !empty($toUpdate[$key]) || $toUpdate[$key] == 0)
+			{
+				$toRequest = $toRequest.", ".$key." = :$key";
+				$valuesArray[$key] = $value;
+			}
+		}
+		$toRequest = $toRequest." WHERE id = :id";
+		$valuesArray["id"] = $id;
+		
+		echo "<pre>\n";
+		print_r($valuesArray);
+		echo $toRequest;
+		echo "</pre>";
+		$request = $db->prepare($toRequest);
+		$request->execute($valuesArray);
+		
+		if($request->rowCount())
+		{
+			$request->closeCursor();
+			return true;
+		}
+		else
+		{
+			$request->closeCursor();
+			return false;
+		}
+	}
 ?>
