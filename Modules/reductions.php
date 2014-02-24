93b4de8922516d598b33714d33cc42871e0787d5
Richard-Degenne <richdeg2@gmail.com>
2014-02-24 21:58:04 +0100

    Structure
    Réalisation de la structure de la page
    
    Implémentation de liste

--- Modules/reductions.php
+++ Modules/reductions.php
@@ -7,6 +7,72 @@
 	 * 
 	 * Page répertoire pour l'affichage, la création, la modification de la liste de réductions disponibles.
 	 */
-?>
 
-<h1>Réductions</h1>
+echo "<h1>Réductions</h1>\n";
+
+	if(isset($_GET["mode"]))
+	{
+		switch($_GET["mode"])
+		{
+			case "liste":
+				liste();
+				break;
+			
+			case "fiche":
+				fiche();
+				break;
+			
+			case "ajouter":
+				ajouter();
+				break;
+			
+			case "modifier":
+				modifier();
+				break;
+			
+			case "supprimer":
+				supprimer();
+				break;
+			
+			default:
+				liste();
+				break;
+		}
+	}
+	else
+	{
+		liste();
+	}
+	
+function liste()
+{
+	echo "<h2>Liste des réductions</h2>\n";
+	$reductions = getAllReductions();
+	
+	echo "<table border=\"1\">\n<tr><th>Description</th><th>Coût</th><th>Type</th><th>Valeur</th><th>Date de début</th><th>Date de fin</th></tr>\n";
+	foreach($reductions as $reduction)
+	{
+		echo "<tr><td>$reduction[description]</td><td>$reduction[cout]</td><td>$reduction[type]</td><td>$reduction[valeur]</td><td>$reduction[debut]</td><td>$reduction[fin]</td></tr>\n";
+	}
+	echo "</table>";
+}
+
+function fiche()
+{
+	echo "<h2>Fiche réduction</h2>";
+}
+
+function ajouter()
+{
+	echo "<h2>Ajout d'un client</h2>\n";
+}
+
+function modifier()
+{
+	echo "<h2>Modification d'un client</h2>\n";
+}
+
+function supprimer()
+{
+	echo "<h2>Suppression d'un client</h2>\n";
+}
