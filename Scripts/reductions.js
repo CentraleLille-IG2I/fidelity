6b4a8b12a27478c0677e730bb320e694d84d6317
Richard-Degenne <richdeg2@gmail.com>
2014-02-25 22:29:32 +0100

    Fix — Regex des champs de recherches
    Optimisation de base pour les champs de recherche.
    
    Pour plus d’infos, voir
    https://github.com/Richard-Degenne/fidelity/issues/3

--- Scripts/reductions.js
+++ Scripts/reductions.js
@@ -1,7 +1,7 @@
 $(function() {
 	$('#recherche').keyup(function() {
 		$('#liste tr:gt(0)').each(function() { // Pour chaque ligne du tableau (hors ligne d'en-tête)
-			if($(this).html().match(new RegExp($('#recherche').val(),'i')) != null) // Si la valeur du champ de recherche est trouvée
+			if($(this).html().match(new RegExp("<td>[^<]*"+$('#recherche').val()+"[^<]*</td>",'i')) != null) // Si la valeur du champ de recherche est trouvée
 			{
 				$(this).css('display','table row'); // Alors on affiche
 			}
